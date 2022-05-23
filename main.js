d991cf04aaf51321f3290c9e7281b1f82957aa3c
Xander Bylo <xanderbylo@gmail.com>
2022-05-23 23:20:39 +0100

    Make playerSelection parameter case-insensitive

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-const playerSelection = 'Rock';
+const playerSelection = 'rock';
 const computerSelection = computerPlay();
 
-console.log(playerSelection, computerSelection);
+console.log(playerSelection, computerSelection)
 
 function computerPlay() {
-    let options = ['Rock', 'Paper', 'Scissors'];
+    let options = ['rock', 'paper', 'scissors'];
     let choice = options[Math.floor(Math.random() * options.length)];
 
     return choice;
 }
 
 function playRound() {
-    if (playerSelection == computerSelection) {
+    if (playerSelection.toLowerCase() === computerSelection) {
         alert("It's a draw!");
-    } else if (computerSelection == 'Paper') {
+    } else if (computerSelection === 'paper') {
         alert('You lose! Paper beats Rock!');
     } else {
         alert('You win! Rock beats Scissors!');
